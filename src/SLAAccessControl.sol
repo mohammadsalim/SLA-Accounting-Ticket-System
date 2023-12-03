406bac29dd0e728102988dbb6238d37a20e185f9
Mohammad Salim <jd2474ev@gmail.com>
2023-12-03 00:33:19 -0500

    Created AccessControl roles for default admin, buyer, and seller

--- src/SLAAccessControl.sol
+++ src/SLAAccessControl.sol
@@ -4,5 +4,20 @@ pragma solidity ^0.8.0;
 import {AccessControl} from "openzeppelin-contracts/access/AccessControl.sol";
 
 contract SLAAccessControl is AccessControl {
+    bytes32 public constant SELLER_ROLE = keccak256("SELLER_ROLE");
+    bytes32 public constant BUYER_ROLE = keccak256("BUYER_ROLE");
 
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function grantSellerRole(address account) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not an admin");
+        _grantRole(SELLER_ROLE, account);
+    }
+
+    function grantBuyerRole(address account) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not an admin");
+        _grantRole(BUYER_ROLE, account);
+    }
 }
