62d7a01d2a8bff926cb14d301978e98e51dc72e7
Mohammad Salim <jd2474ev@gmail.com>
2023-12-02 21:42:34 -0500

    Added a struct to encapsulate all variables related to a given TroubleTicket when submitted

--- src/SLATicketSystem.sol
+++ src/SLATicketSystem.sol
@@ -7,4 +7,15 @@ import "./SLAContractInterface.sol";
 contract SLATicketSystem {
     SLAAccessControl accessControl;
     SLAContractInterface slaContract;
+
+    // Struct for a submitted trouble ticket
+    struct TroubleTicket {
+        uint256 serviceIdentifier;
+        string issueDescription;
+        uint256 timestamp;
+        address buyer;
+        bool isValidated;
+        string sellerComments;
+        uint256 validationTimestamp;
+    }
 }
