8814e4b804ee83e9abb2971a8f72cd6589306386
Mohammad Salim <jd2474ev@gmail.com>
2023-12-03 06:09:44 -0500

    Added SLATicketSystem test setup

---
+++ test/SLATicketSystem.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../src/SLATicketSystem.sol";
+import "../src/SLAAccessControl.sol";
+import "../src/SLACreditsToken.sol";
+import "../src/SLAContract.sol";
+
+contract SLATicketSystemTest is Test {
+    SLATicketSystem ticketSystem;
+    SLAAccessControl accessControl;
+    SLACreditsToken creditsToken;
+    SLAContract slaContract;
+
+    address admin;
+    address buyer;
+    address seller;
+
+    function setUp() public {
+        admin = address(this);  // Test contract is the admin
+        buyer = address(0x1);
+        seller = address(0x2);
+
+        accessControl = new SLAAccessControl();
+        slaContract = new SLAContract();
+        creditsToken = new SLACreditsToken(1e18); // 1 token with 18 decimals
+
+        accessControl.grantRole(accessControl.BUYER_ROLE(), buyer);
+        accessControl.grantRole(accessControl.SELLER_ROLE(), seller);
+
+        ticketSystem = new SLATicketSystem(address(creditsToken), address(accessControl), address(slaContract));
+    }
+}
