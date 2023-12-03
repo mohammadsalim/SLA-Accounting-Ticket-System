870da2bd10b080073e68413dec71f78e0b3b4f83
Mohammad Salim <jd2474ev@gmail.com>
2023-12-02 21:34:47 -0500

    imported SLAAccessControl for roles and permissions and SLAContractInterface to interact with SLA contracts

--- src/SLATicketSystem.sol
+++ src/SLATicketSystem.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./SLAAccessControl.sol";
+import "./SLAContractInterface.sol";
+
 contract SLATicketSystem {
-    
+    SLAAccessControl accessControl;
+    SLAContractInterface slaContract;
 }
